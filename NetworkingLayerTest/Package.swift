0f4b19d58496c93001c2da40cb98f7f27f594d98
PunitNaik <punit.ramchandra@byjus.com>
2024-08-13 14:26:25 +0530

    added the package

---
+++ NetworkingLayerTest/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NetworkingLayerTest",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NetworkingLayerTest",
+            targets: ["NetworkingLayerTest"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NetworkingLayerTest",
+        path: "Sources"),
+        .testTarget(
+            name: "NetworkingLayerTestTests",
+            dependencies: ["NetworkingLayerTest"]),
+    ]
+)
